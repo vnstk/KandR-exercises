7eeec9d5b50bdc5a815a2abcdb46e39a743b6caa
vnstk <75968083+vnstk@users.noreply.github.com>
2025-03-22 13:12:23 -0400

    done with ch3, for now; TODO return to 2-9 and 3-1

--- 2-4and5.c
+++ 2-4and5.c
@@ -28,36 +28,34 @@ int any (const char *const s1, const char *s2) /* Ex 2-5 */
 	return -1;
 }
 
-
 main ()
 {
 	char buf[128];
-#define TESTsqueeze(s1,s2)  \
-		strcpy(buf, s1);    \
-		squeeze(buf,s2);    \
+#define TEST_squeeze(s1,s2)  \
+		strcpy(buf, s1);     \
+		squeeze(buf,s2);     \
 		printf("Ln%3d:   squeeze(\"%s\",\"%s\")  ==>  \"%s\"\n", __LINE__,s1,s2,buf)
-	TESTsqueeze("","farce");
-	TESTsqueeze("antarafacial","farce");
-	TESTsqueeze("antarafacial","xyz");
-	TESTsqueeze("antarafacial","");
-	TESTsqueeze("mississippi","mips RISC");
-	TESTsqueeze("mississippi","iiii");
+	TEST_squeeze("","farce");
+	TEST_squeeze("antarafacial","farce");
+	TEST_squeeze("antarafacial","xyz");
+	TEST_squeeze("antarafacial","");
+	TEST_squeeze("mississippi","mips RISC");
+	TEST_squeeze("mississippi","iiii");
 
-	printf("\n-----------------------------------------------------\n\n");
-
+	printf("\e[35m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\e[0m\n");
 	int answ;
-#define TESTany(s1,s2)   \
+#define TEST_any(s1,s2)   \
 	answ = any(s1,s2);  \
 	if (answ >= 0)   \
 		printf("Ln%3d:   any(\"%s\",\"%s\")  ==>  %d  i.e. '%c'\n",__LINE__,s1,s2,answ,s1[answ]);  \
 	else   \
 		printf("Ln%3d:   any(\"%s\",\"%s\")  ==>  -1\n",__LINE__,s1,s2)
-	TESTany("","farce");
-	TESTany("antarafacial","farce");
-	TESTany("antarafacial","auntie");
-	TESTany("antarafacial","xyz");
-	TESTany("antarafacial","");
-	TESTany("mississippi","mips RISC");
-	TESTany("mississippi","iiii");
-	TESTany("mississippi","pure");
+	TEST_any("","farce");
+	TEST_any("antarafacial","farce");
+	TEST_any("antarafacial","auntie");
+	TEST_any("antarafacial","xyz");
+	TEST_any("antarafacial","");
+	TEST_any("mississippi","mips RISC");
+	TEST_any("mississippi","iiii");
+	TEST_any("mississippi","pure");
 }
