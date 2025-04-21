d6aca643b7ce590a1fa41f750d1da815183f594d
vnstk <75968083+vnstk@users.noreply.github.com>
2025-04-21 11:04:23 -0400

    6-4

---
+++ 6-4.c
@@ -0,0 +1,123 @@
+/* Vainstein K 2025apr21 */
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h> /* for qsort */
+#include <string.h>
+
+char **words;
+int nWords_used=0, nWords_max = 0;
+
+typedef struct {
+	char *_word;
+	int   _count;
+} occurrence_t;
+occurrence_t *items;
+int nItems_used=0, nItems_max = 0;
+
+#define TABSIZE_INI 128
+
+int cmp_word (char const **a, char const **b)
+{
+	return strcmp(*a,*b);
+}
+int cmp_occurrence_byCountDesc (occurrence_t const *a, occurrence_t const *b)
+{
+	return b->_count - a->_count;
+}
+typedef int(*cmpFunc)(void const*,void const*);
+
+void addWord (char const *const s)
+{
+	int const sLen = strlen(s), idest = nWords_used++;
+	if (idest == nWords_max) {
+		if (nWords_max)
+			words = realloc(words, sizeof(char*) * (nWords_max *= 2));
+		else
+			words = malloc(        sizeof(char*) * (nWords_max = TABSIZE_INI));
+	}
+	strcpy( (words[idest] = malloc(sLen+1)) , s);
+}
+occurrence_t *addItem (char *const sStored)
+{
+	int const idest = nItems_used++;
+	if (idest == nItems_max) {
+		if (nItems_max)
+			items = realloc(items, sizeof(occurrence_t) * (nItems_max *= 2));
+		else
+			items = malloc(        sizeof(occurrence_t) * (nItems_max = TABSIZE_INI));
+	}
+	occurrence_t *const occ = &items[idest];
+	occ->_word = sStored; /* No need to strcpy */
+	occ->_count = 1;
+	return occ;
+}
+
+/* Copied from 1-22.c solution; does remove LF from read-in line. */
+bool getline (char *const buf, int const buflen)
+{
+    memset(buf, '\0', buflen);
+    int c=EOF, i;
+    for (i=0; (c=getchar())!=EOF && c!='\n'; ++i) {
+        if (i >= buflen-2) {
+            fprintf(stderr, "Buffer too small.\n");
+			return false;
+        }
+        buf[i] = c;
+    }
+    return c != EOF;
+}
+
+#define WORD_SEPARATOR_CHARS " \t\".,;:()[]<>-?!/"
+#define BUFLEN 1024
+main ()
+{
+	occurrence_t *occ;
+	int i;
+	char line[BUFLEN];
+	char *word, *curr, *prev;
+
+	while (getline(line,sizeof line)) {
+		if (! (word = strtok(line,WORD_SEPARATOR_CHARS)))
+			continue;
+		do {
+			word[0] = tolower(word[0]);
+			addWord(word);
+		} while (word = strtok(NULL,WORD_SEPARATOR_CHARS));
+	}
+	if (! nWords_used)
+		return 1;
+
+	qsort(words, nWords_used, sizeof(char*), (cmpFunc)cmp_word);
+
+	for (occ=NULL, prev=NULL, i = 0; i < nWords_used; ++i) {
+		curr = words[i];
+		if (i == 0) {
+			occ = addItem(curr);
+		} else {
+			if (strcmp(prev,curr)) /* word changed? */
+				occ = addItem(curr);
+			else {
+				occ->_count++;
+				free(curr);
+				continue;
+			}
+		}
+		prev = curr;
+	}
+#ifdef DBG
+	fprintf(stderr, "\e[36m""nWords_used=%d nWords_max=%d , nItems_used=%d nItems_max=%d\e[0m\n",
+	        nWords_used,nWords_max , nItems_used,nItems_max);
+#endif
+
+	qsort(items, nItems_used, sizeof(occurrence_t), (cmpFunc)cmp_occurrence_byCountDesc);
+
+	for (i = 0; i < nItems_used; ++i) {
+		occ = &items[i];
+		printf("%3d %s\n", occ->_count, occ->_word);
+		free(occ->_word);
+	}
+	free(items);
+	free(words);
+}
